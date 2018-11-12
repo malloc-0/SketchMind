0d433dfe19e746e57b78d76132d44a91178fe614
yuxiqian <akaza_akari@sjtu.edu.cn>
2018-11-12 09:10:59 +0800

    Add comments.

--- SketchMind.FrontEnd/JavaScript/struct.js
+++ SketchMind.FrontEnd/JavaScript/struct.js
@@ -1,23 +1,56 @@
+var MapNode = /** @class */ (function () {
+    // 内容。多点内容以数组形式存储。
+    function MapNode(cont) {
+        MapNode.last_id++;
+        this.id = MapNode.last_id;
+        this.content = cont;
+    }
+    MapNode.last_id = 0;
+    return MapNode;
+}());
 var MindMap = /** @class */ (function () {
+    // 表示指向关系的邻接矩阵
     function MindMap(title) {
         this.title = title;
         this.node_count = 0;
     }
     MindMap.prototype.add_node = function (node) {
         this.nodes.push(node);
+        // 先增加一个 node
         this.node_count++;
+        // 增加图的 node count 计数
         for (var i = 0; i < this.adj_mat.length; ++i) {
             this.adj_mat[i].push(0);
+            // 给每条边增加一个结点维度
         }
+    };
+    MindMap.prototype.delete_node = function (id) {
+        for (var i = 0; i < this.adj_mat.length; ++i) {
+            // 遍历所有边
+            if (this.adj_mat[i][id] != 0) {
+                // 如果这条边和所要删除的点有交集（!= 0）
+                this.adj_mat.splice(i);
+                // 就从边矩阵里删掉此边
+                break;
+            }
+            this.adj_mat[i].splice(id);
+            // 如果这条边和所要删除的点没关系
+            // 就只从边矩阵中删除点的维度
+        }
+        this.nodes[id] = undefined;
+        // 清空 nodes 数组中的元素，但保留其索引位置
     };
     MindMap.prototype.add_connection = function (from, to) {
         var new_mat;
         for (var i = 0; i < this.node_count; ++i) {
             new_mat.push(0);
         }
+        // 边矩阵里加入一个全 0 列
         new_mat[from] = -1;
         new_mat[to] = 1;
+        // 设置 -1 1 表示指向关系
         this.adj_mat.push(new_mat);
+        // 把新的边推入矩阵
     };
     MindMap.prototype.cut_connection = function (from, to) {
         var new_mat;
@@ -26,20 +59,13 @@ var MindMap = /** @class */ (function () {
         }
         new_mat[from] = -1;
         new_mat[to] = 1;
+        // 构造出 connection 的矩阵表示
         for (var i = 0; i < this.adj_mat.length; ++i) {
             if (this.adj_mat[i] == new_mat) {
                 this.adj_mat.splice(i);
+                // 找到符合条件的就删除整行
             }
         }
     };
     return MindMap;
-}());
-var MapNode = /** @class */ (function () {
-    function MapNode(cont) {
-        MapNode.last_id++;
-        this.id = MapNode.last_id;
-        this.content = cont;
-    }
-    MapNode.last_id = 0;
-    return MapNode;
 }());
--- SketchMind.FrontEnd/TypeScript/struct.ts
+++ SketchMind.FrontEnd/TypeScript/struct.ts
@@ -1,11 +1,36 @@
 
+class MapNode {
+    static last_id: number = 0;
+    // 静态类型变量，用来给出唯一的全局递增 id。
+
+    id: number;
+    // 每个节点的 id。
+
+    content: [string];
+    // 内容。多点内容以数组形式存储。
+
+    constructor (cont: [string]) {
+        MapNode.last_id++;
+        this.id = MapNode.last_id;
+        this.content = cont;
+    }
+    // 构造器。传入节点的内容（数组形式）
+}
+
+
 class MindMap {
+
     title: string;
+    // 图的标题
+
     nodes: [MapNode];
+    // 结点数组
 
     node_count: number;
+    // 结点计数器
 
     adj_mat: Array<Array<number>>;
+    // 表示指向关系的邻接矩阵
 
     constructor(title: string) {
         this.title = title;
@@ -14,10 +39,30 @@ class MindMap {
 
     add_node(node: MapNode) {
         this.nodes.push(node);
+        // 先增加一个 node
         this.node_count++;
+        // 增加图的 node count 计数
         for (let i: number = 0; i < this.adj_mat.length; ++i) {
             this.adj_mat[i].push(0);
+            // 给每条边增加一个结点维度
         }
+    }
+
+    delete_node(id: number) {
+        for (let i: number = 0; i < this.adj_mat.length; ++i) {
+            // 遍历所有边
+            if (this.adj_mat[i][id] != 0) {
+                // 如果这条边和所要删除的点有交集（!= 0）
+                this.adj_mat.splice(i);
+                // 就从边矩阵里删掉此边
+                break;
+            }
+            this.adj_mat[i].splice(id);
+            // 如果这条边和所要删除的点没关系
+            // 就只从边矩阵中删除点的维度
+        }
+        this.nodes[id] = undefined;
+        // 清空 nodes 数组中的元素，但保留其索引位置
     }
     
     add_connection(from: number, to: number) {
@@ -25,9 +70,14 @@ class MindMap {
         for(let i = 0; i < this.node_count; ++i) {
             new_mat.push(0);
         }
+        // 边矩阵里加入一个全 0 列
+
         new_mat[from] = -1;
         new_mat[to] = 1;
+        // 设置 -1 1 表示指向关系
+
         this.adj_mat.push(new_mat);
+        // 把新的边推入矩阵
     }
 
     cut_connection(from: number, to: number) {
@@ -37,23 +87,14 @@ class MindMap {
         }
         new_mat[from] = -1;
         new_mat[to] = 1;
+        // 构造出 connection 的矩阵表示
+
         for (let i = 0; i < this.adj_mat.length; ++i) {
             if (this.adj_mat[i] == new_mat) {
                 this.adj_mat.splice(i)
+                // 找到符合条件的就删除整行
             }
         }
-    }
-}
-
-class MapNode {
-    static last_id: number = 0;
-    id: number;
-    content: string;
-
-    constructor (cont: string) {
-        MapNode.last_id++;
-        this.id = MapNode.last_id;
-        this.content = cont;
     }
 }
 
