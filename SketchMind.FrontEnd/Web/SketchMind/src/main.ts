d2e39176aef609fb2e04a79bb84c729580a3091c
yuxiqian <akaza_akari@sjtu.edu.cn>
2018-12-04 19:37:53 +0800

    新尝试。

---
+++ SketchMind.FrontEnd/Web/SketchMind/src/main.ts
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false;
+
+
+let VueMaterial = require('vue-material');
+Vue.use(VueMaterial);
+
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: {
+    App
+  }
+});
+
+
+
+
+
+
+
